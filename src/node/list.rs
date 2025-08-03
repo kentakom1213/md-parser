a526263c0b71957e63cb35ce4a877d0380069458
kentakom1213 <kentakom1213@gmail.com>
2025-08-04 00:12:06 +0900

    add: listの追加

--- src/node/list.rs
+++ src/node/list.rs
@@ -1,1 +1,23 @@
 //! リスト
+
+use super::InlineNode;
+
+#[derive(Debug)]
+pub enum ListType {
+    /// 番号なしのリスト
+    List,
+    /// 番号ありのリスト
+    Enum,
+}
+
+#[derive(Debug)]
+pub struct ListNode {
+    pub list_type: ListType,
+    pub contents: Vec<ListItem>,
+}
+
+#[derive(Debug)]
+pub enum ListItem {
+    Inline(InlineNode),
+    List(ListNode),
+}
--- src/node/mod.rs
+++ src/node/mod.rs
@@ -3,6 +3,7 @@ mod codeblock;
 mod document;
 mod heading;
 pub mod inline;
+mod list;
 mod mathblock;
 mod paragraph;
 
@@ -11,5 +12,6 @@ pub use codeblock::CodeBlockNode;
 pub use document::DocumentNode;
 pub use heading::HeadingNode;
 pub use inline::InlineNode;
+pub use list::{ListItem, ListNode, ListType};
 pub use mathblock::MathBlockNode;
 pub use paragraph::ParagraphNode;
